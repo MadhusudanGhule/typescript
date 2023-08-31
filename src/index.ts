68d03101112261706f8b728527c2d4b0a0f44da0
madhusudan <madhusudanghulev@gmail.com>
2023-08-31 14:48:48 +0530

    function in typeScript

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,56 @@ let  sum=(a:number,b:number):number=>{
    return a+b;
 }
 sum(23,43)
+
+
+
+// object in typescript;
+
+// const obj:{
+//     height:number,
+//     weigth:number,
+//     gender:boolean
+// }={
+//     height:123,
+//     weigth:56,
+//     gender:false
+// }
+
+// interface Obj{
+//     height:number,
+//     weigth:number,
+//     gender?:boolean
+// }
+
+// type funcType=(n:number,m:number)=>void
+// interface newObj extends Obj {
+//     scolor:boolean,
+//     func:funcType
+// }
+
+// const getApp:newObj={
+//     height:160,
+//     weigth:50,
+//     scolor:true,
+//     func:(m,n)=>{
+//         console.log(m*n)
+//             }
+// }
+// const kandel:newObj={
+//     height:160,
+//     weigth:50,
+//     scolor:true,
+//     func:(m,n)=>{
+// console.log(m*n)
+//     }
+// }
+// kandel.func(20,40)
+
+// const obj:Obj={
+//     height:160,
+//     weigth:50,
+  
+// }
+
+
+//function in typescript
